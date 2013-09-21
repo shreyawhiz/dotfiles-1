86f34066e522c75dc501eb00130b2a6b357431d7
Ben Truyman <ben@truyman.com>
2013-09-21 00:29:31 -0500

    Unblocks an rbenv path.

--- source/50_devel.fish
+++ source/50_devel.fish
@@ -3,7 +3,7 @@
 
 # Add rbenv executables to path
 set PATH $HOME/.dotfiles/vendor/rbenv/bin $PATH
-#set PATH $HOME/.dotfiles/vendor/rbenv/shims $PATH
+set PATH $HOME/.dotfiles/vendor/rbenv/shims $PATH
 set PATH $HOME/.dotfiles/vendor/ruby-build $PATH
 
 # Initialize rbenv
