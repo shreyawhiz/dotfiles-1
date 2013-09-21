2502ee38a022d5c8feb567da87cee2e08325c5ae
Ben Truyman <ben@truyman.com>
2013-09-21 14:43:42 -0500

    Fixes ruby-build path.

--- source/50_devel.fish
+++ source/50_devel.fish
@@ -1,7 +1,7 @@
 # Add rbenv executables to path
 set PATH $HOME/.dotfiles/vendor/rbenv/bin $PATH
 set PATH $HOME/.dotfiles/vendor/rbenv/shims $PATH
-set PATH $HOME/.dotfiles/vendor/ruby-build $PATH
+set PATH $HOME/.dotfiles/vendor/ruby-build/bin $PATH
 
 # Initialize rbenv
 if type -P rbenv > /dev/null
