626dce9a712061d46c89e380b335559934523fe5
Ben Truyman <ben@truyman.com>
2013-09-23 15:01:32 -0500

    Should fix rbenv issues.

--- source/50_devel.fish
+++ source/50_devel.fish
@@ -1,7 +1,9 @@
+set -U RBENV_ROOT $HOME/.dotfiles/vendor/rbenv
+
 # Add rbenv executables to path
 set PATH $HOME/.dotfiles/vendor/rbenv/bin $PATH
-set PATH $HOME/.dotfiles/vendor/rbenv/shims $PATH
 set PATH $HOME/.dotfiles/vendor/ruby-build/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
 
 # Initialize rbenv
 if type -P rbenv > /dev/null
