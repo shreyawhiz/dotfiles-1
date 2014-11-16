cfac17644b099340685e943df6cb0b7a675189f5
Ben Truyman <ben@truyman.com>
2014-11-15 23:17:53 -0800

    Prevents y/n prompt from apt-get install

--- apt.sh
+++ apt.sh
@@ -7,7 +7,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install \
+sudo apt-get install -y \
   autoconf \
   bc \
   build-essential \
