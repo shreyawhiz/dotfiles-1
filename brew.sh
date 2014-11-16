2c3500f79a7087b373829a4bdbf7039296d21ac1
Ben Truyman <ben@truyman.com>
2014-11-15 22:50:10 -0800

    Adds brew script to install homebrew formulas

---
+++ brew.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+brew update
+brew upgrade
+
+if ! brew cask &> /dev/null; then
+  brew tap phinze/homebrew-cask 2> /dev/null
+  brew install brew-cask 2> /dev/null
+fi
+
+brew install \
+  cloc \
+  coreutils \
+  git \
+  git-extras \
+  go \
+  imagemagick --with-webp \
+  moreutils \
+  rename \
+  tmux \
+  tree \
+  vim \
+  watch \
+  wget
+
+brew cask install \
+  appzapper \
+  divvy \
+  flux \
+  virtualbox
+
+brew cleanup
