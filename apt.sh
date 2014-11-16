0331d658c5656d1bf3d56dcc112d547a113a6835
Ben Truyman <ben@truyman.com>
2014-11-15 22:54:46 -0800

    Adds apt script to install packages on Debian/Ubuntu

---
+++ apt.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+apt-get update
+apt-get upgrade
+
+apt-get install \
+  autoconf \
+  bc \
+  build-essential \
+  git-core \
+  htop \
+  libncurses5-dev \
+  libssl-dev \
+  telnet \
+  tree
