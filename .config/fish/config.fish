f6a93dc3d36f39c794ef17a5e93136f0605a16da
Ben Truyman <ben@truyman.com>
2015-02-24 18:43:31 -0800

    Fixes a long running issue with two $argv being provided to tree

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,7 +27,7 @@ alias .. 'cd ..'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias k 'l'
-alias l 'tree --dirsfirst -aFCNL 1 $argv'
+alias l 'tree --dirsfirst -aFCNL 1'
 alias ll 'tree --dirsfirst -aLpughDFiC 1'
 alias lsd 'll -d'
 
