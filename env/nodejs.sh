55159f4ebba98ff7ef6cf8452944e66de6962c61
Ben Truyman <ben@truyman.com>
2015-10-23 08:55:07 -0700

    Lazy loads nvm

--- env/nodejs.sh
+++ env/nodejs.sh
@@ -1,5 +1,4 @@
 export NVM_DIR="/usr/local/opt/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 npms() {
   npm search --registry=https://registry.npmjs.org $argv
@@ -7,4 +6,9 @@ npms() {
 
 npm-stable() {
   npms $argv | grep -E '2014-[0-9]{2}-[0-9]{2}' | grep -E '[1-9]+\.[0-9]+\.[0-9]+' | sed -e 's/ *$//'
+}
+
+nvm() {
+  . "$NVM_DIR/nvm.sh"
+  nvm $@
 }
