947b6c9b4fca039996898ad65e37f1dac501c923
Ben Truyman <ben@truyman.com>
2015-11-04 17:30:41 -0800

    Stops lazy loading nvm, because it's really annoying

--- env/nodejs.sh
+++ env/nodejs.sh
@@ -1,4 +1,5 @@
 export NVM_DIR="/usr/local/opt/nvm"
+. "$NVM_DIR/nvm.sh"
 
 npms() {
   npm search --registry=https://registry.npmjs.org $argv
@@ -6,9 +7,4 @@ npms() {
 
 npm-stable() {
   npms $argv | grep -E '2014-[0-9]{2}-[0-9]{2}' | grep -E '[1-9]+\.[0-9]+\.[0-9]+' | sed -e 's/ *$//'
-}
-
-nvm() {
-  . "$NVM_DIR/nvm.sh"
-  nvm $@
 }
