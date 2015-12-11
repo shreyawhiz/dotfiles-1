1162179f5ada9d7b561452e8fff19ab4f48188bb
Ben Truyman <ben@truyman.com>
2015-12-10 20:13:31 -0800

    Adds shortcut for Visual Studio Code

--- env/osx.sh
+++ env/osx.sh
@@ -1,4 +1,6 @@
 if [ $(uname -s) = 'Darwin' ]; then
+  code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
+
   alias brewup='brew update && brew upgrade'
   alias dsstore='find . -name "*.DS_Store" -type f -ls -delete'
   alias o='open .'
