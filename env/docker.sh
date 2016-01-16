3b55e421fed7d2b1fc9d49e830d7b4f237d4bc72
Ben Truyman <ben@truyman.com>
2016-01-15 19:56:03 -0800

    Moves dockerfunc stuff to docker env script

--- env/docker.sh
+++ env/docker.sh
@@ -1,6 +1,8 @@
 DOCKER_ENV="$HOME/.dockerenv"
 DOCKER_MACHINE_NAME="default"
 
+# Docker environment helpers
+
 denv() {
   eval "$(cat $DOCKER_ENV)"
 }
@@ -15,6 +17,40 @@ dstop() {
   docker-machine stop $DOCKER_MACHINE_NAME
   rm "$HOME/.dockerenv"
 }
+
+# Docker executables
+
+alpine() {
+  docker run --rm -it bentruyman/alpine "$@"
+}
+
+dshell() {
+  if [ -z "$@" ]; then
+    echo "An image name must be specified"
+    return 1;
+  fi
+  docker run --rm -it "$@" sh
+}
+
+iex() {
+  docker run --rm -it \
+    -v $(pwd):/data/app \
+    -v $HOME/.hex:/root/.hex \
+    -v $HOME/.mix:/root/.mix \
+    -w /data/app \
+    bentruyman/elixir-phoenix-dev iex "$@"
+}
+
+mix() {
+  docker run --rm -it \
+    -v $(pwd):/data/app \
+    -v $HOME/.hex:/root/.hex \
+    -v $HOME/.mix:/root/.mix \
+    -w /data/app \
+    bentruyman/elixir-phoenix-dev mix "$@"
+}
+
+# Initialize Docker environment if one exists
 
 if [[ -f $DOCKER_ENV ]]; then
   denv
