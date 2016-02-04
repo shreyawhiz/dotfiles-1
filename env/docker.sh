59c51902472534357a2aa9b5b845333a81365ff3
Ben Truyman <ben@truyman.com>
2016-02-04 15:51:12 -0800

    Moves dockerfuncs into env/docker

--- env/docker.sh
+++ env/docker.sh
@@ -55,3 +55,27 @@ mix() {
 if [[ -f $DOCKER_ENV ]]; then
   denv
 fi
+
+# Docker command helpers
+
+alpine() {
+  docker run --rm -it bentruyman/alpine "$@"
+}
+
+iex() {
+  docker run --rm -it \
+    -v $(pwd):/data/app \
+    -v $HOME/.hex:/root/.hex \
+    -v $HOME/.mix:/root/.mix \
+    -w /data/app \
+    bentruyman/elixir-phoenix-dev iex "$@"
+}
+
+mix() {
+  docker run --rm -it \
+    -v $(pwd):/data/app \
+    -v $HOME/.hex:/root/.hex \
+    -v $HOME/.mix:/root/.mix \
+    -w /data/app \
+    bentruyman/elixir-phoenix-dev mix "$@"
+}
