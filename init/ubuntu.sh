ecf74c74451746371be5dbfe8bec2c31946e5f10
Ben Truyman <ben@truyman.com>
2016-02-23 22:41:22 -0500

    Fixes Ubuntu initialization process and adds a bit of documentation

--- init/ubuntu.sh
+++ init/ubuntu.sh
@@ -3,10 +3,12 @@ set -e
 
 [[ "$(head -1 < /etc/lsb-release 2> /dev/null)" =~ Ubuntu ]] || return 1
 
-sudo apt-get update
-sudo apt-get install -y software-properties-common
+LINUXBREW="$HOME/.linuxbrew/bin/brew"
 
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
+###############################################################################
+# apt-get
+###############################################################################
+
 sudo add-apt-repository -y ppa:pi-rho/dev
 
 sudo apt-get update
@@ -16,14 +18,56 @@ sudo apt-get install -y \
   autoconf \
   bc \
   build-essential \
+  curl \
+  git \
   git-core \
-  htop \
+  libbz2-dev \
+  libcurl4-openssl-dev \
+  libexpat-dev \
+  libncurses-dev \
   libncurses5-dev \
   libssl-dev \
-  neovim \
+  m4 \
   python-dev \
   python-pip \
   python3-dev \
   python3-pip \
+  ruby \
+  software-properties-common \
   telnet \
-  tree
+  texinfo \
+  tree \
+  zlib1g-dev
+
+###############################################################################
+# Linuxbrew
+###############################################################################
+
+if [[ ! -f "$LINUXBREW" ]]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+fi
+
+ $LINUXBREW update
+ $LINUXBREW install \
+  git-extras \
+  htop \
+  tmux \
+  zsh
+
+###############################################################################
+# Shell
+###############################################################################
+
+ZSH_BIN="$HOME/.linuxbrew/bin/zsh"
+
+# Add ZSH to list of valid shells
+if [[ "$ZSH_BIN" && $(grep -L "$ZSH_BIN" /etc/shells) ]]; then
+  echo "$ZSH_BIN" | sudo tee -a /etc/shells &> /dev/null
+fi
+
+# Change shell to ZSH
+if [[ "$SHELL" != "$ZSH_BIN" ]]; then
+  sudo chsh -s "$ZSH_BIN" "$USER"
+  env "$ZSH_BIN"
+fi
+
