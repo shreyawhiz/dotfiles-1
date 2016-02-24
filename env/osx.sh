ba7c632a70ef3c31e1256f19e7a604074b54c7bc
Ben Truyman <ben@truyman.com>
2016-02-23 20:27:51 -0800

    Updates OS X environment script to be similar in format to Ubuntu

--- env/osx.sh
+++ env/osx.sh
@@ -1,7 +1,7 @@
-if [ $(uname -s) = 'Darwin' ]; then
-  code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
+[[ $(uname -s) = 'Darwin' ]] || return 1
 
-  alias brewup='brew update && brew upgrade'
-  alias dsstore='find . -name "*.DS_Store" -type f -ls -delete'
-  alias o='open .'
-fi
+code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
+
+alias brewup='brew update && brew upgrade'
+alias dsstore='find . -name "*.DS_Store" -type f -ls -delete'
+alias o='open .'
