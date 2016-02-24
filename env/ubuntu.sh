e22e151a72d35ea2d108191eefcbc3cea21fa22b
Ben Truyman <ben@truyman.com>
2016-02-23 20:27:24 -0800

    Adds Ubuntu environment script to add linuxbrew environment vars

---
+++ env/ubuntu.sh
@@ -0,0 +1,5 @@
+[[ "$(head -1 < /etc/lsb-release 2> /dev/null)" =~ Ubuntu ]] || return 1
+
+export PATH="$HOME/.linuxbrew/bin:$PATH"
+export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
