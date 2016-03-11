cb66a96850cbe0dfbca50ca8fe8c1d6408588332
Ben Truyman <ben@truyman.com>
2016-03-10 17:45:16 -0800

    Uses `git ls-tree` for FZF searches

--- env/filesystem.sh
+++ env/filesystem.sh
@@ -17,6 +17,9 @@ unalias rm 2> /dev/null
 if [ -f ~/.fzf.zsh ]; then
   source ~/.fzf.zsh
   # Respect source control ignores
-  export FZF_DEFAULT_COMMAND='ag -l -g ""'
+  export FZF_DEFAULT_COMMAND='
+    (git ls-tree -r --name-only HEAD ||
+     find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
+        sed s/^..//) 2> /dev/null'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
