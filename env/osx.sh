c1f5655371ad76688f52d151619d2fbdf2821579
Ben Truyman <ben@truyman.com>
2016-06-03 19:23:00 -0700

    [osx] adds gcloud ZSH completions

--- env/osx.sh
+++ env/osx.sh
@@ -1,5 +1,11 @@
 [[ $(uname -s) = 'Darwin' ]] || return 1
 
+# Load gcloud completions
+if hash gcloud &> /dev/null; then
+  source "/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+  source "/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
+
 code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
 
 alias brewup='brew update && brew upgrade'
