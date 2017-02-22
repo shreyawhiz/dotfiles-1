17a77dd627dcc9cc123ac54325cd5648bb3d5bc4
Ben Truyman <ben@truyman.com>
2017-02-21 22:57:29 -0800

    [macOS] removes Microsoft VS Code shortcut

--- env/macos.sh
+++ env/macos.sh
@@ -13,8 +13,6 @@ if hash gcloud &> /dev/null; then
   [ -s "$GCLOUD_DIR/completion.zsh.inc" ] && . "$GCLOUD_DIR/completion.zsh.inc"
 fi
 
-code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
-
 alias brewup='brew update && brew upgrade'
 alias dsstore='find . -name "*.DS_Store" -type f -ls -delete'
 alias flushdns="sudo killall -HUP mDNSResponder"
