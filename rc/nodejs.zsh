2a8af7ab4cadc1697471651b57726b03bb948aad
Ben Truyman <ben@truyman.com>
2017-04-11 19:24:48 -0700

    [node] adds 'yarng' shortcut for installing often used global modules
    Closes #4

--- rc/nodejs.zsh
+++ rc/nodejs.zsh
@@ -11,6 +11,20 @@ __() {
     npms "$@" | grep -E "201[5|6]-[0-9]{2}-[0-9]{2}" | grep -E "[1-9]+\.[0-9]+\.[0-9]+" | sed -e 's/ *$//'
   }
 
+  yarng() {
+    local global_packages=(
+      cost-of-modules
+      eslint
+      eslint-config-semistandard
+      eslint-config-standard
+      eslint-plugin-promise
+      eslint-plugin-standard
+      vtop
+    )
+
+    yarn global add ${global_packages}
+  }
+
   # Load nvm
   [[ -s "$NVM_DIR/nvm.sh" ]] && {
     source $NVM_DIR/nvm.sh --no-use
